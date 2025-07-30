d76944232c9496ca8ce95f31951b692176db1b1e
unknown <sailavishnu12@gmail.com>
2025-07-30 17:13:25 +0530

    vishnu'

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -148,7 +148,5 @@ LOGOUT_REDIRECT_URL = '/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
